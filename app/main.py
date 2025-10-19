9376cf9b97f655ad61f25ed5d236c4a03c7bc40a
esther-anierobi <estheranierobi146@gmail.com>
2025-10-19 18:56:04 +0100

    add modified files

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from app import user_route
 app = FastAPI(title="My Profile")
 
 
-app.include_router(
-    user_route.router
-)
+@app.get("/", operation_id="Profile")
+async def root():
+    return {"message": "Welcome to fastAPI and Cat URL"}
+
+
+app.include_router(user_route.router)
--- app/user_route.py
+++ app/user_route.py
@@ -4,12 +4,10 @@ from app.user_service import get_user
 from app.user_schemas import UserResponseSchema
 
 
-router = APIRouter(prefix="/me", tags=[""])
+router = APIRouter(prefix="/me")
 
 
-
-
-@router.get("/", response_model=UserResponseSchema, status_code=status.HTTP_200_OK)
-async def me():
+@router.get("/", operation_id="me", tags=["Profile"], response_model=UserResponseSchema, status_code=status.HTTP_200_OK)
+async def get_all():
     return await get_user()
 
